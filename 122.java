499d7d1f36617feb937782a299d401dff498436a
raahymrehan0 <raahym.rehan.24@ucl.ac.uk>
2025-02-04 17:16:07 +0000

    Best Time to Buy and Sell Stock II

---
+++ 122.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProfit(int[] prices) 
+    {
+        int profit = 0;
+        int i = 0;
+        int hold = prices[0];
+        while (i < prices.length)
+        {
+            if (prices[i] > hold)
+            {
+                profit += prices[i] - hold;
+                hold = prices[i];
+            }
+            else
+            {
+                hold = prices[i];
+            }
+            System.out.println("Hold: " + hold);
+            System.out.println("Profit: " + profit);
+            i++;
+        }
+        return profit;
+    }
+}
