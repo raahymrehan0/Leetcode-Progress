67c7fce0a7358e0de9f226ee00db0b10b0092f27
raahymrehan0 <raahym.rehan.24@ucl.ac.uk>
2025-02-04 16:45:04 +0000

    Best Time to Buy and Sell Stock

---
+++ 121.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxProfit(int[] prices) 
+    {
+        int min_price = 100_000;
+        int max_profit = -1;
+        int profit = 0;
+
+        for (int price : prices)
+        {
+            if (price < min_price)
+            {
+                min_price = price;
+            }
+
+            profit = price - min_price;
+
+            if (profit > max_profit)
+            {
+                max_profit = profit;
+            }
+
+        }
+        return max_profit;
+    }
+}
